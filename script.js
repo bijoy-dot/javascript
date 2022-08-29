1ec4f21ea8a94978e208423d01e77c0138bce812
bijoy-dot <75197033+bijoy-dot@users.noreply.github.com>
2022-08-29 18:33:59 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let js= 'amaiging';
+// if(js== "amaiging") alert(js);
+// console.log(js);
+
+// let x = 11+10;
+// x+=10;
+// console.log(x);
+
+
+// let mass = 78;
+// let height = 1.69;
+
+// let johnBmi= mass / height ** 2 ;
+// console.log(johnBmi);
+// let m = 95;
+// let h = 1.88;
+
+// let markBmi= m / h ** 2 ;
+// console.log(markBmi);
+
+// let x = johnBmi>markBmi
+// console.log(x);
+
+// First exercise is complete  ********************************done ********************************
+const age = 19;
+const licanse = age =>18;
+
+if(licanse){
+  console.log("it is too easy to get licanse from embassy🤞");
+}
