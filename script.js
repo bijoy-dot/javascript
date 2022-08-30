fb2cc4e0a1ccda97c7c21a83bf5827c966940697
bijoy-dot <75197033+bijoy-dot@users.noreply.github.com>
2022-08-31 02:02:50 +0600

    2nd day

--- script.js
+++ script.js
@@ -22,9 +22,92 @@
 // console.log(x);
 
 // First exercise is complete  ********************************done ********************************
-const age = 19;
-const licanse = age =>18;
+// const age = 19;
+// const licanse = age =>18;
 
-if(licanse){
-  console.log("it is too easy to get licanse from embassy🤞");
-}
+// if(licanse){
+//   console.log("it is too easy to get licanse from embassy🤞");
+// }
+
+// function fruitProcessor(apples, oranges){
+//   console.log(apples, oranges);
+//   const juice = 'Juice with ${}';
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+
+// var a = 5;
+// var b = 10;
+// console.log(`Sum is equal: ${a + b}`);
+
+// function calcAge(birthYear){
+//   return 2037 - birthYear;
+// }
+// const age = calcAge(1991);
+// console.log(` age is ${age}`);
+
+// const data= function(birthYear){
+//   return 1991- birthYear;
+// }
+
+// const getAge = birthYear => 2037-birthYear;
+// const setAge = getAge(1991);
+// console.log(setAge);
+
+// const retairmentAge = birthYear => {
+//   const age = 2037-birthYear;
+//   const retairement = 65 - age;
+//   return `retairement date is ${retairement}`;
+// }
+// console.log(retairmentAge(1991));
+
+// const retairmentAge = function (birthYear){ 
+// return 2037-birthYear;
+// }
+// const retairementYear = function (birthYear,birthname){
+//   const age = retairmentAge(birthYear)
+//   const retairement = 65-age;
+//   if(retairement>0){
+//     return retairement;
+//   }else{
+//     return -1;
+//   }
+// }
+// console.log(retairementYear(1991));
+// console.log(retairementYear(1970));
+
+// const array = [
+//   "bijoy",
+//   "biswas",
+//   "chicken",
+//   "cake",
+//   "cake2",
+//   "cake3",
+//   "cake4",
+//   "chicken2",
+//   "chicken3",  
+// ];
+// const bijoy ={
+//   firstName: 'bijoy',
+//   lastName: 'biswas',
+//   email: 'johndoe@gmail.com',
+//   phone: '555-555-5555',
+//   birthdate: 1991,
+//   age : function(){
+//     // console.log(this);
+//     return 2037- bijoy.birthdate;
+//   }
+// }
+// console.log(bijoy.age());
+// console.log(array);
+// console.log(bijoy);
+// console.log(bijoy.firstName);
+// const nameKey ="Name";
+// console.log(bijoy['first'+ nameKey]);
+// console.log(bijoy['last'+ nameKey]);
+// const name = prompt ("what the fuck you want to write");
+// console.log(bijoy[name]);
+// for(i=0; i<array.length; i++){
+//   console.log(array[i]);
+// }
